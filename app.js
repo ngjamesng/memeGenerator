18eed1c4f05420acb9f48532ee06078d1cfe31e6
ngjamesng <just.james.ng@gmail.com>
2019-11-03 17:41:23 -0800

    add caption function and clear input after submission

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	function submit(event) {
 		event.preventDefault();
 		createMeme(url.value, topText.value, bottomText.value);
+		clearInputFields();
 	}
-
 	// add img, top text, and bottom text to new image
-	function createMeme(url, top, bottom) {
+	function createMeme(url, topText, bottomText) {
 		// create meme/div
 		let meme = document.createElement("div");
 		meme.setAttribute("class", "meme");
@@ -24,5 +24,21 @@ document.addEventListener("DOMContentLoaded", () => {
 		let image = document.createElement("img");
 		image.setAttribute("src", url);
 		meme.appendChild(image);
+		// add top text
+		let top = document.createElement("div");
+		top.setAttribute("class", "caption top-caption");
+		top.innerText = topText;
+		meme.appendChild(top);
+		// add bottom text
+		let bottom = document.createElement("div");
+		bottom.setAttribute("class", "caption bottom-caption");
+		bottom.innerText = bottomText;
+		meme.appendChild(bottom);
+	}
+	// clear input fields
+	function clearInputFields() {
+		url.value = "";
+		topText.value = "";
+		bottomText.value = "";
 	}
 });
