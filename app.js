f5d12c92afa52c9cc3e0fc9c23a7174925349e8b
ngjamesng <just.james.ng@gmail.com>
2019-11-03 16:33:17 -0800

    add meme creation function

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
 	// select form, url, top text, and bottom text
 	const form = document.querySelector("form"),
 		url = document.querySelector("#image-url"),
-		topText = document.querySelector("#top-text"),
-		bottomText = document.querySelector("#bottom-text");
+		topText = document.querySelector("#top-text-input"),
+		bottomText = document.querySelector("#bottom-text-input");
+	// individual images
+	const submittedMemes = document.querySelector("#submitted-memes");
 
 	// handle Submit
 	form.addEventListener("submit", submit);
 	function submit(event) {
-		let imgVal = urlField.value,
-			topTextVal = topText.val,
-			bottomTextVal = bottomText.val;
-
-		// event.preventDefault();
+		event.preventDefault();
+		createMeme(url.value, topText.value, bottomText.value);
 	}
 
 	// add img, top text, and bottom text to new image
+	function createMeme(url, top, bottom) {
+		// create meme/div
+		let meme = document.createElement("div");
+		meme.setAttribute("class", "meme");
+		submittedMemes.appendChild(meme);
+		// append image to the meme/div
+		let image = document.createElement("img");
+		image.setAttribute("src", url);
+		meme.appendChild(image);
+	}
 });
